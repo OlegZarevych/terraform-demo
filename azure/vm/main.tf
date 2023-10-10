17c9622e4f4023ebae3115c56d172034947a290d
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2023-10-10 11:06:54 +0300

    update steps for docker install

--- azure/vm/main.tf
+++ azure/vm/main.tf
@@ -148,7 +148,11 @@ resource "azurerm_linux_virtual_machine" "linuxvm" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
-      "sudo apt-get install docker.io -y",
+      "sudo apt-get install curl apt-transport-https ca-certificates software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+      "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable'",
+      "sudo apt update",
+      "sudo apt install docker-ce",
       "sudo docker run -d -p 80:80 httpd"
     ]
   }
