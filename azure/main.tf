ee6c80814ef3da92c9a1d62cea4bbce9a14c0b60
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2023-10-12 23:54:10 +0300

    add samples with loops

--- azure/main.tf
+++ azure/main.tf
@@ -7,3 +7,15 @@ resource "local_file" "private_key" {
   content  = module.linux_vm.tls_private_key
   filename = "private_key.pem"
 }
+
+# module "linux_vm_with_count" {
+#   source = "./vm"
+#   count = 2
+#   name   = "demo${count.index}"
+# }
+
+# module "linux_vm_with_foreach" {
+#   for_each = toset(["demo-each1", "demo-each2"])
+#   source = "./vm"
+#   name   = each.key
+# }
